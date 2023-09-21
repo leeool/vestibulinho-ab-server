098de210baf9b863fa2eb95b8514a5e8d4d1abd2
leeool <lgonsalez02@gmail.com>
2023-09-21 00:19:18 -0300

    fix: show http method

--- src/routes.ts
+++ src/routes.ts
@@ -5,7 +5,7 @@ const router = Router()
 
 router.get("/blog-post", blogPostController.index)
 router.post("/blog-post", blogPostController.store)
-router.put("/blog-post/:id", blogPostController.show)
+router.get("/blog-post/:id", blogPostController.show)
 router.put("/blog-post/:id", blogPostController.update)
 router.delete("/blog-post/:id", blogPostController.delete)
 
